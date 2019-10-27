b2e3bc5c4f68e729e05e39cd924e093dc80823dd
Thomas Sullivan <tdsullivan812@gmail.com>
2019-10-27 18:43:05 -0400

    Imported some 3D models for narrative purposes, and have been starting to work on animations

---
+++ Assets/Animations/Curtain_Open.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curtain_Open : MonoBehaviour
+{
+    Animator animator;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position[2] >= 660)
+        {
+            animator.SetTrigger("Open");
+        }
+    }
+}
---
+++ Assets/First Person Drifter Controller/Scripts/CurtainOpen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurtainOpen : MonoBehaviour
+{
+
+    public bool curtOpen;
+    public GameObject curtains;
+    // Start is called before the first frame update
+    void Start()
+    {
+        curtOpen = false;
+        curtains = GameObject.Find("stage_curtains_1297");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((this.transform.position[2] >= 0) && curtOpen == false)
+        {
+            curtOpen = true;
+            curtains.GetComponent<Animation>().Play();
+        }
+    }
+}
